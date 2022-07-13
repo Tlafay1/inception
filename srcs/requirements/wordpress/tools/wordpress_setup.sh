1638d5d750849b8b86d89d3e5e193a5e0bd6bc0f
Tlafay1 <tlclash12@gmail.com>
2022-07-13 17:40:49 +0200

    saved

---
+++ srcs/requirements/wordpress/tools/wordpress_setup.sh
@@ -0,0 +1,7 @@
+wp core download
+
+# wp core install --url="localhost"  \
+# 				--title="Blog Title" \
+# 				--admin_user="tlafay" \
+# 				--admin_password="inception" \
+# 				--admin_email="blabla@blabla.fr"
