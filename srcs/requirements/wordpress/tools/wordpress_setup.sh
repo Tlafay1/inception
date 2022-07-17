f5b907f76defdbccd819a2fa3fc16f2bd3b8be8a
Tlafay1 <tlclash12@gmail.com>
2022-07-17 16:55:22 -0400

    Volumes in progress
    mariadb keeps crashing with volumes

--- srcs/requirements/wordpress/tools/wordpress_setup.sh
+++ srcs/requirements/wordpress/tools/wordpress_setup.sh
@@ -1,7 +1,9 @@
-wp core download
+wp core download --allow-root
 
 # wp core install --url="localhost"  \
 # 				--title="Blog Title" \
 # 				--admin_user="tlafay" \
 # 				--admin_password="inception" \
 # 				--admin_email="blabla@blabla.fr"
+
+php-fpm7.3 -F
