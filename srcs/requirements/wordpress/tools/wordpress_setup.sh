39607d26735a42c7aedd470b58e9d37a78e877a0
Tlafay1 <tlclash12@gmail.com>
2022-07-20 11:05:03 -0400

    mariadb seems fine, nginx on its way

--- srcs/requirements/wordpress/tools/wordpress_setup.sh
+++ srcs/requirements/wordpress/tools/wordpress_setup.sh
@@ -1,9 +1,16 @@
 wp core download --allow-root
 
-# wp core install --url="localhost"  \
-# 				--title="Blog Title" \
-# 				--admin_user="tlafay" \
-# 				--admin_password="inception" \
-# 				--admin_email="blabla@blabla.fr"
+wp core config --dbhost=mariadb \
+			--dbname=wordpress \
+			--dbuser=tlafay \
+			--dbpass=tlafaypass \
+			--allow-root
+
+wp core install --url="localhost"  \
+				--title="Blog Title" \
+				--admin_user="tlafay" \
+				--admin_password="inception" \
+				--admin_email="blabla@blabla.fr"\
+				--allow-root
 
 php-fpm7.3 -F
